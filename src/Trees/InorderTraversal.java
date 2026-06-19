ac8b52a6e77b7584a9b6503b71cf1615f9b860d5
dhruvkhanna <dhruvkhanna2163@gmail.com>
2026-06-19 11:39:14 +0530

    init commit

---
+++ src/Trees/InorderTraversal.java
@@ -0,0 +1,39 @@
+package Trees;
+
+public class InorderTraversal {
+        //the basic difference between preorder and inorder is
+      //preorder prints root first then left and then right values
+    //inorder prints left then root and then right
+
+        // Inorder Traversal Method
+        public static void printInorder(Node node) {
+
+            if (node == null) {
+                return;                    // Base Case
+            }
+
+            // Step 1: Left subtree
+            printInorder(node.left);
+
+            // Step 2: Root (Current Node)
+            System.out.print(node.data + " ");
+
+            // Step 3: Right subtree
+            printInorder(node.right);
+        }
+
+        public static void main(String[] args) {
+
+            // Tree banana
+            Node root = new Node(10);
+            root.left = new Node(5);
+            root.left.left = new Node(3);
+            root.left.right = new Node(7);
+            root.right = new Node(15);
+            root.right.right = new Node(20);
+
+            System.out.println("Inorder Traversal:");
+            printInorder(root);
+        }
+    }
+
--- src/Trees/PreorderTraversal.java
+++ src/Trees/PreorderTraversal.java
@@ -9,13 +9,23 @@ public class PreorderTraversal {
         printPreorder(root.left);//fir left subtree
         printPreorder(root.right);//fir right subtree
     }
+    //so we understand the\at first it will traverse the entire tree with left subtrees
     static void main() {
-        Node root = new Node(10);
-        root.left = new Node(5);
-        root.left.left = new Node(3);
-        root.left.right = new Node(7);
-        root.right = new Node(15);
-        root.right.right = new Node(20);
+        Node root = new Node(2);
+        root.left = new Node(3);
+        root.left.left = new Node(5);
+        root.left.left.left = new Node(7);
+        root.left.left.left.left = new Node(8);
+        root.left.left.left.left.left = new Node(10);
+        root.left.left.left.left.left.left= new Node(11);
+        root.left.left.left.right = new Node(9);
+        root.left.right = new Node(6);
+        root.left.right.right = new Node(3);
+        root.right = new Node(4);
+        root.left.left.left.left.right = new Node(12);
+        root.right.left = new Node(5);
+        root.right.left.left = new Node(13);
+
 
         System.out.println("Preorder Traversal (Pura Tree):");
         printPreorder(root);
