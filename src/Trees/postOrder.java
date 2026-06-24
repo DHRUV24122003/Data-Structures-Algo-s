1fb5d4895db2f6f194c46d391e812c13005173bf
dhruvkhanna <dhruvkhanna2163@gmail.com>
2026-06-24 10:46:31 +0530

    init commit

--- src/Trees/postOrder.java
+++ src/Trees/postOrder.java
@@ -1,15 +1,51 @@
 package Trees;
+import java.util.*;
 
 public class postOrder {
-    public static void PrintpostOrder(Node root){
-        if(root == null){
-            return; //agar node nahi hai to wapas jao
+    //recursive method
+//    public static void PrintpostOrder(Node root){
+//        if(root == null){
+//            return; //agar node nahi hai to wapas jao
+//        }
+//
+//        PrintpostOrder(root.left);
+//        PrintpostOrder(root.right);
+//        System.out.print(root.data + " ");
+//    }
+
+    //iterative method
+
+
+        public void postOrderIterative(Node root){
+            if(root == null)
+                return;
+
+
+        Stack<Node> stack1 = new Stack<>();
+        Stack<Node> stack2 = new Stack<>();
+
+        stack1.push(root);
+        while(!stack1.isEmpty()) {
+            Node current = stack1.pop();//stack 1 me delete ho gaya
+            stack2.push(current);//stack 2 me push kar diya
+            if (current.left != null) {
+                stack1.push(current.left);
+            }
+            if (current.right != null) {
+                stack1.push(current.right);
+            }
+        }
+        while(!stack2.isEmpty()){
+            System.out.print( stack2.pop().data + " ");
         }
 
-        PrintpostOrder(root.left);
-        PrintpostOrder(root.right);
-        System.out.print(root.data + " ");
     }
+
+
+
+
+
+
     static void main() {
         Node root = new Node(2);
         root.left = new Node(3);
@@ -26,9 +62,9 @@ public class postOrder {
         root.right.left = new Node(5);
         root.right.left.left = new Node(13);
 
-
+        postOrder obj = new postOrder();
         System.out.println("Preorder Traversal (Pura Tree):");
-        PrintpostOrder(root);
+        obj.postOrderIterative(root);
     }
 }
 
