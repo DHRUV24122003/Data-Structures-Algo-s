14cb7f70d1c45754ea329a04885eb43aeef887a9
dhruvkhanna <dhruvkhanna2163@gmail.com>
2026-07-20 22:18:58 +0530

    init commit

---
+++ src/BackTracking/MazeWithObstacles.java
@@ -0,0 +1,36 @@
+package BackTracking;
+
+import java.util.ArrayList;
+
+public class MazeWithObstacles {
+    static void main() {
+
+
+        boolean[][] board = {
+                {true, true, true},
+                {true, true, true},
+                {true, true, true}
+        };
+
+       pathRestrictions(" ", board, 0, 0);
+    }
+
+    static void pathRestrictions(String p, boolean[][] maze, int r, int c) {
+        if (r == maze.length-1 && c == maze[0].length-1 ) {
+            System.out.println(p);
+            return;
+        }
+
+        if(maze[r][c]){
+            return;
+        }
+
+
+        if (r < maze.length-1) {
+            pathRestrictions(p + "D", maze, r + 1, c );
+        }
+        if (c< maze[0].length-1) {
+            pathRestrictions(p + "R", maze, r , c+1);
+        }
+    }
+}
--- src/BackTracking/PrintPaths.java
+++ src/BackTracking/PrintPaths.java
@@ -1,10 +1,15 @@
 package BackTracking;
 
+import java.util.ArrayList;
+
 public class PrintPaths {
     static void main() {
         //System.out.println("Total no of paths are : " + (count(3,3)));
-        path(" ",3,3);
+        System.out.println(pathRet(" ", 3, 3));
     }
+
+
+
     // here this code is only to find out number of paths
 //    static int count(int r, int c){
 //        if(r==1||c==1){
@@ -14,23 +19,33 @@ public class PrintPaths {
 //        int right = count(r,c-1);
 //        return left+right;
 //    }
-   //it is for printing the total number of paths
-    static void path (String p, int r , int c ){
-        if(r==1 && c==1){
-            System.out.println(p);
-            return;
+    //it is for printing the total number of paths
+    static ArrayList<String> pathRet(String p, int r, int c) {
+        if (r == 1 && c == 1) {
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
         }
 
+        ArrayList<String> list = new ArrayList<>();
 
-        if(r>1){
-            path(p + 'D',r-1,c);
+        //if we have tpo check the path diangonally as well
+
+        if(r>1 && c>1){
+            list.addAll(pathRet(p+"D", r-1, c-1));
+        }
+        if (r > 1) {
+            list.addAll(pathRet(p + "V", r - 1, c));
         }
 
-        if(c>1){
-            path(p+ 'R',r,c-1);
+        if (c > 1) {
+            list.addAll(pathRet(p + "H", r, c - 1));
         }
+        return list;
+    }
 
-}
+
+
 }
 
 
